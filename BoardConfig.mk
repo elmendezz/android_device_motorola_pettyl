26cdb7585c4fe82d45ec3f20b70ddacff70f1775
elmendezz <165435968+elmendezz@users.noreply.github.com>
2026-02-20 04:07:52 -0800

    update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -22,7 +22,7 @@ TARGET_ARCH := arm
 TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
-TARGET_CPU_VARIANT := cortex-a53
+TARGET_CPU_VARIANT := generic
 
 # Platform
 TARGET_BOARD_PLATFORM := msm8937
