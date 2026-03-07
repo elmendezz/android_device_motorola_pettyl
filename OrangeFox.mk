ed916cbd86b63762fa460b65ac3e4719920d667c
elmendezz <165435968+elmendezz@users.noreply.github.com>
2026-03-07 00:33:40 -0800

    update OrangeFox.mk

--- OrangeFox.mk
+++ OrangeFox.mk
@@ -21,13 +21,34 @@ $FOX_ASH_IS_BASH := 1
 #FOX_ENABLE_AROMA_COMMON := 1
 
 # Descarga el binario automáticamente durante la compilación
-$FOX_USE_AROMA_FM := 1
+#FOX_USE_AROMA_FM := 1
+
+# Opciones específicas para dispositivos antiguos/Go Edition
+# Para ahorrar espacio en el recovery.img si es muy pesado
+#FOX_DRASTIC_SIZE_REDUCTION := 1 
+
+# Personalización (Opcional)
+#FOX_VERSION := R11.1
+#OF_MAINTAINER := elmendezz
+
+
+# ----------- FIX THE BOOTLOOP - TODO ------------- ↓
+# OrangeFox
+# Indica que estamos compilando OrangeFox
+FOX_RECOVERY_INSTALL_PARTITION := /dev/block/bootdevice/by-name/recovery
+FOX_REPLACE_BUSYBOX_UTILS := 1
+FOX_USE_BASH_SHELL := 1
+FOX_USE_NANO_EDITOR := 1
+FOX_USE_TAR_BINARY := 1
+FOX_USE_SED_BINARY := 1
+FOX_USE_XZ_UTILS := 1
+FOX_ASH_IS_BASH := 1
 
 # Opciones específicas para dispositivos antiguos/Go Edition
 # Para ahorrar espacio en el recovery.img si es muy pesado
 FOX_DRASTIC_SIZE_REDUCTION := 1 
 
 # Personalización (Opcional)
-FOX_VERSION := R11.1
-OF_MAINTAINER := elmendezz
+FOX_VERSION := "R11.1"
+OF_MAINTAINER := "elmendezz"
 
