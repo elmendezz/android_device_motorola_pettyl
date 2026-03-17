061c65d23a12427ed8e14d79624e0ea8101c4de6
elmendezz <165435968+elmendezz@users.noreply.github.com>
2026-03-17 03:00:44 -0700

    update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -21,7 +21,7 @@ LOCAL_PATH := device/motorola/pettyl
 
 # Architecture
 TARGET_ARCH := arm
-TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_ARCH_VARIANT := armv8-a
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_CPU_VARIANT := cortex-a53
