d34a93ac860e9ba32667399dd285fcdbef5cee4d
elmendezz <165435968+elmendezz@users.noreply.github.com>
2026-03-17 02:15:14 -0700

    update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,8 +15,8 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/omni_pettyl.mk \
+    $(LOCAL_DIR)/twrp_pettyl.mk \
     $(LOCAL_DIR)/full_pettyl.mk
 
 COMMON_LUNCH_CHOICES := \
-    omni_pettyl-eng
+    twrp_pettyl-eng
